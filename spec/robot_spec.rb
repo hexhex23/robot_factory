dcb67081a8b020890230149f3189e8e91e747a10
Jerel Lim <j.limster@gmail.com>
2017-04-26 11:40:55 +0800

    update

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -4,7 +4,17 @@ describe Robot do
   describe 'Initialization' do
     it 'should create new robot with name' do
       r1 = Robot.new
-      expect(r1.name).to eq('AB123')
+      expect(r1).to have_attributes(name: 'AB123')
     end
   end
+
+
+  describe 'Reset' do
+    it 'should create new name for robot' do
+      r1 = Robot.new
+      r1.reset
+      expect(r1.name).not_to eq('AB123')
+    end
+  end
+
 end
