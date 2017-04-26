cfe93e885ab3b9353636ea2bc4808a0cb632bbb7
hexhex23 <mariawilson23@gmail.com>
2017-04-26 11:41:05 +0800

    test updated

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,10 +1,18 @@
 require_relative '../robot.rb'
 
 describe Robot do
+  before(:context) do
+    @r1 = Robot.new('AB123')
+    @r2 = Robot.new('CD321')
+  end
   describe 'Initialization' do
     it 'should create new robot with name' do
-      r1 = Robot.new
-      expect(r1.name.length).to exist
+      @r1 = Robot.new
+      expect(@r1).to be_instance_of(Robot)
+    end
+    it 'check that robot names are random' do
+      @r2 = Robot.new
+      expect(r1.name).not_to eql(r2.name)
     end
   end
 end
