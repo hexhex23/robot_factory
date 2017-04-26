e272d0069249280958bb305f8e3c6ac4f047f501
hexhex23 <mariawilson23@gmail.com>
2017-04-26 10:26:53 +0800

    first test

---
+++ spec/robot_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../robot.rb'
+
+describe Robot do
+  describe 'Initialization' do
+    it 'should create new robot with name' do
+      r1 = Robot.new
+      expect(r1.name.length).to exist
+    end
+  end
+end
